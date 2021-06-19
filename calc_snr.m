32fe12da9fa86b66106c49129c5e0b54e3a76d70
Yin-Ping Cho <63432757+YinPing-Cho@users.noreply.github.com>
2021-06-19 18:23:46 +0800

    Delete calc_snr.m

--- calc_snr.m
+++
@@ -1,8 +0,0 @@
-filelist = dir('*.wav');
-allFileNames = {filelist(:).name};
-for k = 1 : length(allFileNames)
-    filename = allFileNames{k};
-    x = audioread(filename);
-    SNR = snr(x);
-    disp(compose('%s SNR: %.6f dB', filename, SNR));
-end
